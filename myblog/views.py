6a019cc5b83539a34acf507d170b1737c7838bc3
Abiodun Akinbile <147928937+klickabbey@users.noreply.github.com>
2024-11-09 21:09:15 +0000

    added new update

--- myblog/views.py
+++ myblog/views.py
@@ -1,8 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-from  django.http import HttpResponse
+from myblog.models import Post
 # Create your views here.
 def post_list(request):
-    from blog.models import Post
-def post_list(request):
     posts = Post.objects.all()
-    return HttpResponse(f"List of posts: {'posts':posts} ")
+    return render(request, 'myblog/post_list.html',{'posts':posts})
